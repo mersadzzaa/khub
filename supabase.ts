eb43490b3aa40e17fb8f45b8c4e469b8be19f54e
mersadzzaa <parsconsoleofficial@gmail.com>
2025-12-31 15:55:54 +0330

    Update supabase.ts

--- supabase.ts
+++ supabase.ts
@@ -5,6 +5,7 @@ import { createClient } from "@supabase/supabase-js";
 // به داشبورد Supabase بروید -> Settings -> API
 // ------------------------------------------------------------------
 const SUPABASE_URL = "https://xnhtfdcqzhrmxipgizoq.supabase.co";
-const SUPABASE_ANON_KEY = "api";
+const SUPABASE_ANON_KEY = "sb_publishable_4Qolzb9gVF_50MyLIz2KfQ_6w9yJkZ0";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
